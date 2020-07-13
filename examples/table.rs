e58f8fbb3409f17c9e4cc624f4a1183f92f445e2
Carlos Eduardo Melo <ceduardo.melo@redime.com.br>
2020-07-13 13:58:44 -0300

    feat: adds a Table widget
    This widget is meant to be used to display tabular data. Its current implementation is very simple, and is susceptible to variations in cell height, rendering the table columns with different row heights.

---
+++ examples/table.rs
@@ -0,0 +1,65 @@
+use birog::table::Table;
+use druid::widget::Label;
+use druid::{AppLauncher, Data, Lens, LocalizedString, Widget, WidgetExt, WindowDesc};
+use std::sync::Arc;
+
+#[derive(Clone, Data, Lens)]
+struct AppData {
+  data: Arc<Vec<Person>>,
+}
+
+#[derive(Clone, Data, Lens)]
+struct Person {
+  first_name: String,
+  last_name: String,
+  phone: String,
+}
+
+fn main() {
+  let window = WindowDesc::new(ui_builder)
+    .window_size((1024., 500.))
+    .title(LocalizedString::new("custom-widget-demo-window-title").with_placeholder("Table Test"));
+
+  AppLauncher::with_window(window)
+    .use_simple_logger()
+    .launch(data_builder())
+    .expect("launch failed");
+}
+
+fn ui_builder() -> impl Widget<AppData> {
+  Table::new()
+    .with_column(
+      "First Name",
+      || Label::new(|person: &Person, _env: &_| person.first_name.clone()),
+      200.0,
+    )
+    .with_column(
+      "Last Name",
+      || Label::new(|person: &Person, _env: &_| person.last_name.clone()),
+      200.0,
+    )
+    .with_flex_column(
+      "Phone Number",
+      || Label::new(|person: &Person, _env: &_| person.phone.clone()),
+      1.0,
+    )
+    .lens(AppData::data)
+    .debug_paint_layout()
+}
+
+fn data_builder() -> AppData {
+  AppData {
+    data: Arc::new(vec![
+      Person {
+        first_name: "John".to_string(),
+        last_name: "Doe".to_string(),
+        phone: "555-1234".to_string(),
+      },
+      Person {
+        first_name: "Jane".to_string(),
+        last_name: "Smith".to_string(),
+        phone: "555-0000".to_string(),
+      },
+    ]),
+  }
+}
---
+++ src/table.rs
@@ -0,0 +1,100 @@
+use std::cmp::Ordering;
+
+use druid::widget::{Flex, Label, List, ListIter, SizedBox};
+use druid::{
+  BoxConstraints, Data, Env, Event, EventCtx, LayoutCtx, LifeCycle, LifeCycleCtx, PaintCtx, Point,
+  Rect, Size, UpdateCtx, Widget, WidgetPod,
+};
+use std::marker::PhantomData;
+
+pub struct Table<C, T> {
+  root: WidgetPod<T, Flex<T>>,
+  data_type: PhantomData<C>,
+}
+
+struct Column<T> {
+  is_flex: bool,
+  param: f64,
+  widget: WidgetPod<T, Flex<T>>
+}
+
+impl<C: Data, T: ListIter<C>> Table<C, T> {
+  pub fn new() -> Self {
+    Self {
+      root: WidgetPod::new(Flex::row()),
+      data_type: Default::default()
+    }
+  }
+
+  pub fn with_column<W: Widget<C> + 'static>(
+    mut self,
+    title: impl Into<String>,
+    closure: impl Fn() -> W + 'static,
+    width: f64,
+  ) -> Self {
+    let widget = Flex::column()
+      .with_child(SizedBox::new(Label::new(title.into())).width(width))
+      .with_child(SizedBox::new(List::new(closure)).width(width));
+
+    self.root.widget_mut().add_child(widget);
+
+    self
+  }
+
+  pub fn with_flex_column<W: Widget<C> + 'static>(
+    mut self,
+    title: impl Into<String>,
+    closure: impl Fn() -> W + 'static,
+    param: f64,
+  ) -> Self {
+    let widget = Flex::column()
+        .with_child(Label::new(title.into()))
+        .with_child(List::new(closure));
+
+    self.root.widget_mut().add_flex_child(widget, param);
+
+    self
+  }
+}
+
+impl<C: Data, T: ListIter<C>> Widget<T> for Table<C, T> {
+  fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut T, env: &Env) {
+    self.root.event(ctx, event, data, env)
+  }
+
+  fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &T, env: &Env) {
+    self.root.lifecycle(ctx, event, data, env)
+  }
+
+  fn update(&mut self, ctx: &mut UpdateCtx, _old_data: &T, data: &T, env: &Env) {
+    self.root.update(ctx, data, env)
+  }
+
+  fn layout(&mut self, ctx: &mut LayoutCtx, bc: &BoxConstraints, data: &T, env: &Env) -> Size {
+    let mut width = bc.min().width;
+    let mut height = 0.0;
+
+    let mut paint_rect = Rect::ZERO;
+
+    let child_bc = BoxConstraints::new(
+      Size::new(bc.min().width, 0.0),
+      Size::new(bc.max().width, std::f64::INFINITY),
+    );
+    let child_size = self.root.layout(ctx, &child_bc, data, env);
+    let rect = Rect::from_origin_size(Point::new(0.0, height), child_size);
+    self.root.set_layout_rect(ctx, data, env, rect);
+    paint_rect = paint_rect.union(self.root.paint_rect());
+
+    width = width.max(child_size.width);
+    height = child_size.height;
+
+    let my_size = bc.constrain(Size::new(width, height));
+    let insets = paint_rect - Rect::ZERO.with_size(my_size);
+    ctx.set_paint_insets(insets);
+    my_size
+  }
+
+  fn paint(&mut self, ctx: &mut PaintCtx, data: &T, env: &Env) {
+    self.root.paint(ctx, data, env)
+  }
+}
